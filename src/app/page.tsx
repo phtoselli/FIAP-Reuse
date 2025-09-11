037f301c527ba8cf3f076887fadffd75978bf2f0
Phtoselli <phtoselli.dev@gmail.com>
2025-09-11 18:25:32 -0300

    remove home and better redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,6 @@
-"use client";
-
+import { redirect } from "next/navigation";
 import { Routes } from "@/types/routes";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push(Routes.LOGIN);
-  }, [router]);
-
-  return (
-    <div>
-      <p>Home</p>
-    </div>
-  );
+  redirect(Routes.LOGIN);
 }
